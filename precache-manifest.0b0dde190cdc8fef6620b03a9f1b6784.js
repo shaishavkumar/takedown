f07088e959585986a3ece2a74ed9991345ad42f0
shaishavkumar <shaishavkumar@users.noreply.github.com>
2020-09-12 13:04:46 +0530

    Updates

---
+++ precache-manifest.0b0dde190cdc8fef6620b03a9f1b6784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d89472a5e2c433f918050ca106e7b00",
+    "url": "/takedown/index.html"
+  },
+  {
+    "revision": "e628944b2be4ef4ee283",
+    "url": "/takedown/static/css/main.b38b7943.chunk.css"
+  },
+  {
+    "revision": "b0d2eed031edf7fe8fe7",
+    "url": "/takedown/static/js/2.872c39d9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/takedown/static/js/2.872c39d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e628944b2be4ef4ee283",
+    "url": "/takedown/static/js/main.a5a193b8.chunk.js"
+  },
+  {
+    "revision": "327dee82da250ed5a003",
+    "url": "/takedown/static/js/runtime-main.6f61395e.js"
+  },
+  {
+    "revision": "a973b1e3d2885d716fab297ed458bd6a",
+    "url": "/takedown/static/media/logo.a973b1e3.png"
+  }
+]);
